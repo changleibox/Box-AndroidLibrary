08303521fd91c2b2e7aa346c1b23f4c9bba93421
Box <changleibox@sina.com>
2017-07-07 14:07:31 +0800

    增加限制输入的inputfilter

--- boxlibrary/src/main/java/net/izhuo/app/library/text/ValueInputFilter.java
+++ boxlibrary/src/main/java/net/izhuo/app/library/text/ValueInputFilter.java
@@ -3,45 +3,80 @@ package net.izhuo.app.library.text;
 import android.text.InputFilter;
 import android.text.Spanned;
 import android.text.TextUtils;
-import android.util.Log;
 
-import java.text.NumberFormat;
+import java.util.Arrays;
 
 public class ValueInputFilter implements InputFilter {
 
-    private static final String TAG = ValueInputFilter.class.getSimpleName();
+    private static final String EMPTY = "";
 
-    private double mMinValue = Double.MIN_VALUE;
-    private double mMaxValue = Double.MAX_VALUE;
+    private final double mMinValue;
+    private final double mMaxValue;
 
-    @SuppressWarnings("deprecation")
     public ValueInputFilter(double minValue, double maxValue) {
         this.mMinValue = minValue;
         this.mMaxValue = maxValue;
-
-        NumberFormat format = NumberFormat.getInstance();
-        Log.i(TAG, "minValue = " + format.format(mMinValue));
-        Log.i(TAG, "maxValue = " + format.format(mMaxValue));
     }
 
     @Override
     public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
-        StringBuilder builder = new StringBuilder(dest);
         if (TextUtils.isEmpty(source)) {
-            builder.delete(dstart, dend);
-        } else {
-            builder.insert(dstart, source.toString().toCharArray());
+            return null;
         }
 
+        char[] chars = source.toString().toCharArray();
+        for (int i = source.length(); i >= 0; i--) {
+            char[] tmpChars = Arrays.copyOf(chars, i);
+            if (!isExceed(new StringBuilder(dest).insert(dstart, tmpChars))) {
+                return new String(tmpChars);
+            }
+        }
+        return EMPTY;
+    }
+
+    private boolean isExceed(CharSequence valueStr) {
         double value;
         try {
-            value = Double.valueOf(builder.toString());
+            value = Double.valueOf(valueStr.toString());
         } catch (Exception e) {
-            return "";
+            return true;
         }
-        if (value > mMaxValue || value < mMinValue) {
-            return "";
-        }
-        return null;
+        return value > mMaxValue || value < mMinValue;
     }
+
+    // private static final String TAG = ValueInputFilter.class.getSimpleName();
+    //
+    // private double mMinValue = Double.MIN_VALUE;
+    // private double mMaxValue = Double.MAX_VALUE;
+    //
+    // @SuppressWarnings("deprecation")
+    // public ValueInputFilter(double minValue, double maxValue) {
+    //     this.mMinValue = minValue;
+    //     this.mMaxValue = maxValue;
+    //
+    //     NumberFormat format = NumberFormat.getInstance();
+    //     Log.i(TAG, "minValue = " + format.format(mMinValue));
+    //     Log.i(TAG, "maxValue = " + format.format(mMaxValue));
+    // }
+    //
+    // @Override
+    // public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+    //     StringBuilder builder = new StringBuilder(dest);
+    //     if (TextUtils.isEmpty(source)) {
+    //         builder.delete(dstart, dend);
+    //     } else {
+    //         builder.insert(dstart, source.toString().toCharArray());
+    //     }
+    //
+    //     double value;
+    //     try {
+    //         value = Double.valueOf(builder.toString());
+    //     } catch (Exception e) {
+    //         return "";
+    //     }
+    //     if (value > mMaxValue || value < mMinValue) {
+    //         return "";
+    //     }
+    //     return null;
+    // }
 }
